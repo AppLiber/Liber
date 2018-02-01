b6b83465f21d49de4fc83a5f19b20886c25187e8
MatheusEstudos <matheusqueirozestudos@gmail.com>
2018-02-01 13:44:30 -0200

    [ADD]Ajustado admin.py.  models aparecendo.

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Perfil, Estante
+from .models import Perfil, Estante, AvaliaLido, Emprestimo, AvaliaEmpréstimo
 
 admin.site.register(Perfil)
 admin.site.register(Estante)
+admin.site.register(AvaliaLido)
+admin.site.register(Emprestimo)
+admin.site.register(AvaliaEmpréstimo)
