e22f38c500091506c05c66157530732087aed7c2
naiieandrade <andradenaiara@hotmail.com>
2018-02-02 12:20:07 -0200

    [UPDATE] Atualiza cadastro usuario no admin

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from livros.models import Livro
+from livros.models import Livro, Categoria
 
 from django.contrib.auth.models import User
 
@@ -17,7 +17,7 @@ NOTA_LIDO = (
     ('05', 5),
     ('06', 6),
     ('07', 7),
-    ('08', 8 ),
+    ('08', 8),
     ('09', 9),
     ('10', 10),
 )
@@ -31,15 +31,15 @@ class Perfil(models.Model):
     telefone = models.CharField(max_length=15, null=True)
     data_de_nascimento = models.DateField(blank=True)
     sexo = models.CharField(max_length=1, choices=SEXO_USUARIO, blank=True , null=True)
+    categorias_preferidas = models.ManyToManyField("livros.Categoria")
 
+#    preferido1 = models.OneToOneField('livros.Categoria' , on_delete=models.CASCADE, related_name="Preferido1" , default = "drama", unique= True, null=True, blank=True)
+#    preferido2 = models.OneToOneField('livros.Categoria' , on_delete=models.CASCADE, related_name="Preferido2" , default = "", unique= True) #,, null=True, blank=True)
+#    preferido3 = models.OneToOneField('livros.Categoria' , on_delete=models.CASCADE, related_name="Preferido3" , default = "", unique= True) #,, null=True, blank=True)
+#    preferido4 = models.OneToOneField('livros.Categoria' , on_delete=models.CASCADE, related_name="Preferido4" , default = "", unique= True, null=True, blank=True)
+#    preferido5 = models.OneToOneField('livros.Categoria' , on_delete=models.CASCADE, related_name="Preferido5" , default = "", unique= True, null=True, blank=True)
 
-    preferido1 = models.ForeignKey('livros.Categoria' , on_delete=models.CASCADE, related_name="Preferido1" , default = "", unique= True)
-    preferido2 = models.ForeignKey('livros.Categoria' , on_delete=models.CASCADE, related_name="Preferido2" , default = "", unique= True, blank=True)
-    preferido3 = models.ForeignKey('livros.Categoria' , on_delete=models.CASCADE, related_name="Preferido3" , default = "", unique= True, blank=True)
-    preferido4 = models.ForeignKey('livros.Categoria' , on_delete=models.CASCADE, related_name="Preferido4" , default = "", unique= True, blank=True)
-    preferido5 = models.ForeignKey('livros.Categoria' , on_delete=models.CASCADE, related_name="Preferido5" , default = "", unique= True, blank=True)
-
-    #imagem_perfil = models.ImageField(upload_to='imagem_perfil/', default='imagem_perfil/user.png')
+    imagem_perfil = models.ImageField(upload_to='imagem_perfil/', default='imagem_perfil/user.png')
 
     #contatos = models.ManyToManyField('self')
 
@@ -56,25 +56,39 @@ class Perfil(models.Model):
     def email(self):
         return self.usuario.email
 """
-
+STATUS_LIVRO = (
+    ('D', 'Disponível'),
+    ('E', 'Emprestado'),
+)
 
 class Estante(models.Model):
 
     nome = models.CharField(max_length=30, blank=False , default="Estante")
     # perfil_dono
-    perfil_dono = models.OneToOneField(Perfil, on_delete=models.CASCADE)
+    perfil_dono = models.OneToOneField(Perfil, on_delete=models.CASCADE, null=True) #ver esse nulo depois ??!!
     livros = models.ManyToManyField('livros.Livro') # Livro
-    disponível = models.BooleanField()
+    #livros_estante = models.ManyToManyField('livros.Livro', through='EstanteLivro')
+    status = models.CharField(max_length=1, choices=STATUS_LIVRO, blank=True , null=True)
+    #status = models.ForeignKey('self', null=True, related_name)
 
     def __str__(self):
         return self.nome
 
+
+"""
+class EstanteLivro(models.Model):
+    estante = models.ForeignKey(Estante, on_delete=models.CASCADE)
+    livro_adicionado = models.ForeignKey(Estante.livros, on_delete=models.CASCADE)
+    data_adicionado = models.DateField(auto_now=True)
+    status = models.CharField(max_length=1, choices=STATUS_LIVRO, blank=True , null=True)
+"""
+
 class AvaliaLido (models.Model):
     # perfil_dono
     lido = models.BooleanField()
-    perfil_Avaliador= models.ForeignKey(Perfil, on_delete=models.CASCADE)
+    perfil_Avaliador= models.OneToOneField(Perfil, on_delete=models.CASCADE)
     livros = models.ManyToManyField('livros.Livro') # Livro
-    nota = models.IntegerField(choices=NOTA_LIDO)
+    nota = models.IntegerField(choices=NOTA_LIDO) # ver se nao é char
 
     def __str__(self):
         return self.nota
@@ -82,8 +96,8 @@ class AvaliaLido (models.Model):
 
 class Emprestimo (models.Model):
     perfil_do_dono = models.OneToOneField(Perfil, on_delete=models.CASCADE, related_name="solicitado")
-    perfil_solicitante = models.OneToOneField(Perfil, on_delete=models.CASCADE, related_name="solicitante")
-    livro_emprestado = models.OneToOneField(Estante, on_delete=models.CASCADE)
+    perfil_solicitante = models.OneToOneField(Perfil, on_delete=models.CASCADE, related_name="solicitante") #on_delete=models.CASCADE,
+    livro_emprestado = models.OneToOneField(Estante, on_delete=models.CASCADE) # livro da estante
     data_emprestimo= models.DateField(auto_now=True)
     data_devolucao= models.DateField()
 
@@ -91,7 +105,7 @@ class Emprestimo (models.Model):
         return self.perfil_do_dono
 
 
-class AvaliaEmpréstimo (models.Model):
+class AvaliaEmprestimo (models.Model):
     nota = models.IntegerField(choices=NOTA_LIDO)
     Emprestimo_avaliado = models.OneToOneField(Emprestimo, on_delete=models.CASCADE)
 
