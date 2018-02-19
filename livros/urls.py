ef43a4b3bab647228836591448d36c0df2027251
naiieandrade <andradenaiara@hotmail.com>
2018-02-19 19:56:43 -0300

    [ADD] Template detalhe livro usario logado

--- livros/urls.py
+++ livros/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib.auth.decorators import login_required
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -13,6 +14,7 @@ urlpatterns = [
     #path('', views.LivroIndex.as_view(), name='livros_index'),
     path('', views.ListLivros.as_view(), name='lista_livros'),
     path('<int:pk>/', views.LivroDetail.as_view(), name='livros_detail'),
+    path('<int:pk>/detail', login_required(views.LivroDetailLogado.as_view()), name='livros_detail_logado'),
     path('new', views.LivroCreate.as_view(), name='livros_new'),
     path('<int:pk>/edit', views.LivroUpdate.as_view(), name='livros_edit'),
     path('<int:pk>/delete', views.LivroDelete.as_view(), name='livros_delete'),
