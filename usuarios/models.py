298945a46c450884f1362c6247b6be596e981176
naiieandrade <andradenaiara@hotmail.com>
2018-02-24 14:31:31 -0300

    [ADD] Mostra mensagem de pedido de empréstimo

--- usuarios/models.py
+++ usuarios/models.py
@@ -97,8 +97,8 @@ class Emprestimo (models.Model):
     data_emprestimo= models.DateField(auto_now=True)
     data_devolucao= models.DateField(null=True)
     status_emprestimo = models.CharField(max_length=2, choices=STATUS_EMPRESTIMO, blank=False , null=False, default='S')
-    mensagem_de_quem_pede = models.TextField(null=True, blank=True)
-    mensagem_resposta = models.TextField(null=True, blank=True)
+    mensagem_de_quem_pede = models.CharField(null=True, blank=True, max_length=200)
+    mensagem_resposta = models.CharField(null=True, blank=True,max_length=200)
 
     def __str__(self):
         return'Dono >{}, para {},{}'.format(self.perfil_do_dono.usuario.username, self.perfil_solicitante, self.livro_emprestado)
