a1c71cc22911fa2b44b0ee871a37d120bf576904
naiieandrade <andradenaiara@hotmail.com>
2018-03-04 23:23:19 -0300

    [UPDATE] Botão de adicionar autor

--- livros/forms.py
+++ livros/forms.py
@@ -1,11 +1,14 @@
 from django.forms import ModelForm
+from django import forms
 
 from .models import Autor, Categoria, Livro
 
 class LivroForm(ModelForm):
+    #autores = forms.ModelMultipleChoiceField(queryset=Autor.objects.all(), required=False, widget=forms.CheckboxSelectMultiple())
+    #categorias = forms.ModelMultipleChoiceField(queryset=Categoria.objects.all(), required=False, widget=forms.CheckboxSelectMultiple())
     class Meta:
         model = Livro
-        fields = ['titulo', 'editora', 'isbn10', 'isbn13', 'descricao', 'paginas', 'autores', 'edicao', 'ano', 'categorias', 'imagem']
+        fields = ['titulo', 'editora', 'isbn10', 'isbn13', 'descricao', 'paginas', 'autores', 'edicao', 'ano', 'categorias' ] #, 'imagem']
 
 
 class AutorForm(ModelForm):
